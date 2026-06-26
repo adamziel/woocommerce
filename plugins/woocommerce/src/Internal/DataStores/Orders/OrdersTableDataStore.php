e3d8f7dfb4a7be166c49820ca1d102c3b9c33372
Adam Zieliński <adam@adamziel.com>
2026-06-26 04:30:07 +0200

    Add HPOS created_via order index

--- plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableDataStore.php
+++ plugins/woocommerce/src/Internal/DataStores/Orders/OrdersTableDataStore.php
@@ -3398,7 +3398,8 @@ CREATE TABLE $operational_data_table_name (
 	discount_total_amount decimal(26,8) NULL,
 	recorded_sales tinyint(1) NULL,
 	UNIQUE KEY order_id (order_id),
-	KEY order_key (order_key)
+	KEY order_key (order_key),
+	KEY created_via_order_id (created_via, order_id)
 ) $collate;
 CREATE TABLE $meta_table (
 	id bigint(20) unsigned auto_increment primary key,
